9befe2a666527a373536023e7902055c60aca863
anicxxl <kopotiyvlad@gmail.com>
2023-05-25 18:02:16 +0300

    added main slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function () {
-	
-})
+"use strict"
+
+const swiper = new Swiper(".main-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    //пауза между прокруткой
+    delay: 3000,
+    //закончить на последнем слайде
+    stopOnLastSlide: false,
+    //Отключить после ручного переключения
+    disableOnInteraction: false
+  }
+});
